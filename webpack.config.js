b16e892a10e5a408de8388164d359f273fac4c76
Jan Nakladal <mojeto1@gmail.com>
2019-06-01 23:44:34 +1200

    Configure webpack to build index.html and link js file to it.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: true,
+            chunks: ['main'],
+            filename: 'index.html'
+        }),
+    ],
 };
